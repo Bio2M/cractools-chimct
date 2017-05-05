0518d68b3893a9544ed593f41a44af3c39259eb3
benoit <benoit.guibert@free.fr>
2017-05-05 14:47:19 +0200

    modify slurm parameters

--- snake-slurm.sh
+++ snake-slurm.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+# loading environment
+[ -f /data/bin/init.sh ] && . /data/bin/init.sh 
+
+# slurm log directory
+slurm_log_dir=output/logs/slurm
+[ -d "$slurm_log_dir" ] || mkdir -p $slurm_dir
+
+
 { time snakemake \
     -ap \
     -j 99 \
     --cluster-config cluster.yml \
-    --cluster "sbatch \
+    --cluster 'sbatch \
         -A {cluster.account} \
         -p {cluster.partition} \
-        -n {cluster.n}" ;
+        -J {cluster.jobname} \
+        -e $slurm_log_dir/{cluster.output} \
+        -o /dev/null \
+        -n {cluster.n}' ;
 } 2>output/snakemake.log
 
-
-# Move slurm output in logs directory
-
-slurm_dir=output/logs/slurm
-
-[ -d "$slurm_dir" ] || mkdir -p $slurm_dir
-mv slurm-*.out $slurm_dir
